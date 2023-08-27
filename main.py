a6704411537d99021e3c80496ec19e5590ffad67
sebastian <sebramirezcordero@gmail.com>
2023-08-27 13:04:24 -0600

    API dice la posicion de cada agente y su id

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 model = Puerto()
 
+# cada request nos dice la posicion en el siguiente step.
 @app.route('/index')
 def index():
     model.step()
     datos = model.get_data()
+    # no todos los valores de id son numeros entonces hay que serializarlo
     serialized_data = json.dumps(datos)
     return serialized_data
 
