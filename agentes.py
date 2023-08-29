da7c0bd1dc33d9af01ed31044f1ea95f8aa587e2
sebramirez44 <sebramirezcordero@gmail.com>
2023-08-28 19:04:18 -0600

    casi todos los agentes listos

--- agentes.py
+++ agentes.py
@@ -373,8 +373,54 @@ class Puerto(Model):
           "arregloContenedores": contenedores_dict
         }
         data[agent.unique_id] = agent_data
-      # data[agent.unique_id] tiene que ser un objeto con valores definidos
-      # data[agent.unique_id] = agent.pos
+      elif isinstance(agent, GruaPortico):
+        contenedores_dict = []
+        for contenedorOrg in agent.contenedores:
+          contenedor = {
+            "weight": contenedorOrg.weight,
+            "size" : contenedorOrg.size,
+            "status" : contenedorOrg.status
+          }
+          contenedores_dict.append(contenedor)
+        agent_data = {
+          "cargando": agent.cargando,
+          "cantContenedores": len(agent.contenedores),
+          "arregloContenedores": contenedores_dict
+        }
+        data[agent.unique_id] = agent_data
+      elif isinstance(agent, Explanada):
+        contenedores_dict = []
+        for contenedorOrg in agent.contenedores:
+          contenedor = {
+            "weight": contenedorOrg.weight,
+            "size": contenedorOrg.size,
+            "status": contenedorOrg.status
+          }
+          contenedores_dict.append(contenedor)
+        agent_data = {
+          "cantContenedores": len(agent.contenedores),
+          "arregloContenedores": contenedores_dict
+        }
+        data[agent.unique_id] = agent_data
+      elif isinstance(agent,  GruaRTG):
+        contenedores_dict = []
+        # gruaRTG tiene atributo contenedores con mayuscula, tal vez cambiarlo si es mas problema
+        for contenedorOrg in agent.Contenedores:
+          contenedor = {
+            "weight": contenedorOrg.weight,
+            "size": contenedorOrg.size,
+            "status": contenedorOrg.status
+          }
+          contenedor_dict.append(contenedor)
+        x,y = agent.pos
+        agent_data = {
+          "cantContenedores": len(agent.Contenedores),
+          "destino": agent.destino,
+          "posX": x,
+          "posY": y,
+          "arregloContenedores" : contenedores_dict
+        }
+        data[agent.unique_id] = agent_data
       
     return data
 
